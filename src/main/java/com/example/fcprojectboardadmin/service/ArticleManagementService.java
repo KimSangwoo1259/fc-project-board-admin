3f9dd3f8d634d89ff8b74564771a5e85d25109c1
KimSangwoo12 <rlatkddn2902@gmail.com>
2023-08-29 00:56:42 +0900

    #26 - 회원관리 기능 컨트롤러, 서비스, 뷰 구현

--- src/main/java/com/example/fcprojectboardadmin/service/ArticleManagementService.java
+++ src/main/java/com/example/fcprojectboardadmin/service/ArticleManagementService.java
@@ -36,7 +36,8 @@ public class ArticleManagementService {
 
     public ArticleDto getArticle(Long articleId){
         URI uri = UriComponentsBuilder.fromHttpUrl(projectProperties.board().url() + "/api/articles/" + articleId)
-                .build()//TODO 불완전하다.
+                .queryParam("projection", "withUserAccount")
+                .build()
                 .toUri();
 
         ArticleDto response = restTemplate.getForObject(uri, ArticleDto.class);
